86080ce4c5d0f535a72a6acb367cd6fb5bf7ae0c
Ryan Hubbuck <rhubbuck@gmail.com>
2021-08-07 17:53:44 -0400

    updated to range and styled font

--- app.js
+++ app.js
@@ -1,15 +1,17 @@
 document.getElementById('calculator').onchange = ()=> {
     const results = document.getElementById('results');
     let totalBill = Number(document.getElementById('total').value);
-    let tipPercent = document.getElementById('tip-percent').value;
+    let tipPercent = document.getElementById('range').value;
     const totalWithTip = document.getElementById('total-with-tip');
     const tipAmount = document.getElementById('tip-amount');
+    const tipOutput = document.getElementById('tip-output');
 
     let tipTotal = totalBill * (tipPercent / 100);
     
     if (tipTotal !== 0) 
         {tipAmount.innerHTML = `$${tipTotal.toFixed(2)}`;
-         results.style.display = 'block';}
+         results.style.display = 'block';
+        tipOutput.innerHTML = `${tipPercent}%`;}
 
     let billTotal = totalBill + tipTotal;
     
