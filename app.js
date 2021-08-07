8c306b9c8f5c56e6d884c1c7a9ff486adc080409
Ryan Hubbuck <rhubbuck@gmail.com>
2021-08-07 17:12:21 -0400

    Final commit

--- app.js
+++ app.js
@@ -1,13 +1,18 @@
 document.getElementById('calculator').onchange = ()=> {
+    const results = document.getElementById('results');
     let totalBill = Number(document.getElementById('total').value);
     let tipPercent = document.getElementById('tip-percent').value;
-    let tipTotal = totalBill * (tipPercent / 100);
-    const tipAmount = document.getElementById('tip-amount');
-    if (tipTotal !== 0) 
-        {tipAmount.innerHTML = tipTotal.toFixed(2)};
-    let billTotal = totalBill + tipTotal;
     const totalWithTip = document.getElementById('total-with-tip');
-    if (tipTotal !== 0) 
-        {totalWithTip.innerHTML = billTotal.toFixed(2);}
+    const tipAmount = document.getElementById('tip-amount');
 
+    let tipTotal = totalBill * (tipPercent / 100);
+    
+    if (tipTotal !== 0) 
+        {tipAmount.innerHTML = `$${tipTotal.toFixed(2)}`;
+         results.style.display = 'block';}
+
+    let billTotal = totalBill + tipTotal;
+    
+    if (tipTotal !== 0) 
+        {totalWithTip.innerHTML = `$${billTotal.toFixed(2)}`;}
 };
